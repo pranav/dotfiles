ae42335365c956f1a8b34329912032475cc4b174
hyfi <hyfi@smeebot.(none)>
2014-01-06 03:27:28 -0500

    Actually symlinking file now

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 cutstring="DO NOT EDIT BELOW THIS LINE"
 
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/gitconfig ~/.gitconfig
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qa
